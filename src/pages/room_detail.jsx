765d7b9e95ffabf2b3c3fb794220d258440719bc
Zawadirugenyi <zawadirugenyi@gmail.com>
2024-07-18 11:16:38 +0300

    addind features in room detail

--- src/pages/room_detail.jsx
+++ src/pages/room_detail.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Heading, Text, Image, Stack, Center, Spinner,Button, Link } from '@chakra-ui/react';
+import { Box, Heading, Text, Image, Stack, Center, Spinner, Button, Link, Modal, ModalOverlay, ModalContent, ModalCloseButton, ModalBody } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // Requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 const RoomDescription = () => {
     const { roomNumber } = useParams();
     const [loading, setLoading] = useState(true);
     const [roomDescription, setRoomDescription] = useState(null);
     const [error, setError] = useState(null);
+    const [zoomedImage, setZoomedImage] = useState(null);
 
     useEffect(() => {
         const fetchRoomDescription = async () => {
@@ -41,6 +44,14 @@ const RoomDescription = () => {
         fetchRoomDescription();
     }, [roomNumber]);
 
+    const handleImageClick = (imageSrc) => {
+        setZoomedImage(imageSrc);
+    };
+
+    const closeZoom = () => {
+        setZoomedImage(null);
+    };
+
     if (loading) {
         return (
             <Center height="300px">
@@ -59,39 +70,98 @@ const RoomDescription = () => {
     }
 
     return (
-        <Box p={9}>
-            <Box p={9} shadow="md" borderWidth="1px">
-                <Heading as="h2" size="lg" mb={4}>
-                    Room {roomDescription.room_number} Description
-                </Heading>
-
-                <Stack direction="row" spacing={4} mb={4}>
-                    <Image src={`http://127.0.0.1:8000${roomDescription.sitting_room_image}`} alt="Sitting Room" boxSize="630px" w="900px" />
-                    <Stack direction="column" spacing={3} mb={4}>
-                        <Image src={`http://127.0.0.1:8000${roomDescription.bedroom_image}`} alt="Bedroom" boxSize="200px" />
-                        <Image src={`http://127.0.0.1:8000${roomDescription.kitchen_image}`} alt="Kitchen" boxSize="200px" />
-                        <Image src={`http://127.0.0.1:8000${roomDescription.bathroom_image}`} alt="Bathroom" boxSize="200px" />
+        <Box p={9} marginTop="25px">
+            <Stack direction="row" spacing={10}>
+                 <Box shadow="md" borderWidth="1px" p={9}>
+                    <Stack direction="row" spacing={4} mb={4}>
+                        <Image
+                            src={`http://127.0.0.1:8000${roomDescription.sitting_room_image}`}
+                            alt="Sitting Room"
+                            boxSize="400px"
+                            w="800px"
+                            cursor="pointer"
+                            onClick={() => handleImageClick(`http://127.0.0.1:8000${roomDescription.sitting_room_image}`)}
+                        />
+                        <Stack direction="column" spacing={3} mb={4} h="400px">
+                            <Image
+                                src={`http://127.0.0.1:8000${roomDescription.bedroom_image}`}
+                                alt="Bedroom"
+                                boxSize="200px"
+                                cursor="pointer"
+                                onClick={() => handleImageClick(`http://127.0.0.1:8000${roomDescription.bedroom_image}`)}
+                            />
+                            <Image
+                                src={`http://127.0.0.1:8000${roomDescription.kitchen_image}`}
+                                alt="Kitchen"
+                                boxSize="200px"
+                                cursor="pointer"
+                                onClick={() => handleImageClick(`http://127.0.0.1:8000${roomDescription.kitchen_image}`)}
+                            />
+                            <Image
+                                src={`http://127.0.0.1:8000${roomDescription.bathroom_image}`}
+                                alt="Bathroom"
+                                boxSize="200px"
+                              
+                                cursor="pointer"
+                                onClick={() => handleImageClick(`http://127.0.0.1:8000${roomDescription.bathroom_image}`)}
+                            />
+                        </Stack>
                     </Stack>
-                </Stack>
+                </Box>
+                <Box width="40%">
+                    <Box shadow="md" borderWidth="1px" p={9}>
+                        <Heading as="h2" size="lg" mb={4}>
+                            Room {roomDescription.room_number} Description
+                        </Heading>
+                        <Text mb={4}>
+                            Description: {roomDescription.description}
+                        </Text>
+                     <Text mb={4} fontWeight="bold" fontSize="27px">
+                       Price: Ksh {roomDescription.price}
+                      </Text>
 
-                <Text mb={4}>
-                    Description: {roomDescription.description}
-                </Text>
-                <Text>
-                    Price: Ksh {roomDescription.price}
-                </Text>
-              <Button
-              as={Link}
-              to="/login"
-              mt={4}
-              width="400px"
-              bg="#0097b2"
-              color="white"
-              _hover={{ bg: "#073d47" }}
-            >
-              Book Now
-            </Button>
-            </Box>
+                      
+                        <Button
+                        as={Link}
+                        to="/login/"
+                        mt={4}
+                        width="100%"
+                        bg="#0097b2"
+                        color="white"
+                        _hover={{ bg: "#073d47" }}
+                        
+                    >
+                        Book Now
+                    </Button>
+
+                    </Box>
+                </Box>
+            </Stack>
+
+            {zoomedImage && (
+                <Modal isOpen={!!zoomedImage} onClose={closeZoom} isCentered>
+                    <ModalOverlay />
+                    <ModalContent maxWidth="57%">
+                        <ModalCloseButton />
+                        <ModalBody  shadow="md">
+                            <Carousel showThumbs={false}>
+                                <div>
+                                    <img src={`http://127.0.0.1:8000${roomDescription.sitting_room_image}`} alt="Sitting Room" />
+                                </div>
+                                <div>
+                                    <img src={`http://127.0.0.1:8000${roomDescription.bedroom_image}`} alt="Bedroom" />
+                                </div>
+                                <div>
+                                    <img src={`http://127.0.0.1:8000${roomDescription.kitchen_image}`} alt="Kitchen" />
+                                </div>
+                                <div>
+                                    <img src={`http://127.0.0.1:8000${roomDescription.bathroom_image}`} alt="Bathroom" />
+                                </div>
+                            </Carousel>
+                        </ModalBody>
+                    </ModalContent>
+                </Modal>
+            )}
         </Box>
     );
 };
