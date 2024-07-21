78c92a80168e9ac9592bdd2c5debfa11de404b2e
Zawadirugenyi <zawadirugenyi@gmail.com>
2024-07-21 21:40:34 +0300

    adjusting booking page

--- src/pages/booking.jsx
+++ src/pages/booking.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Box, FormControl, FormLabel, Input, Button, Select, useToast, Grid, GridItem, Image} from '@chakra-ui/react';
+import { Box, FormControl, FormLabel, Input, Button, Select, useToast, Grid, GridItem } from '@chakra-ui/react';
 import axios from 'axios';
 import backgroundImage from '../Components/Assets/Room2.webp'; 
 
@@ -20,8 +20,8 @@ const Booking = () => {
     // Fetch rooms and tenants from the API
     const fetchRoomsAndTenants = async () => {
       try {
-        const roomsResponse = await axios.get('http://127.0.0.1:8000/api/rooms/?room__number=${encodedRoomNumber}');
-        const tenantsResponse = await axios.get('http://127.0.0.1:8000/api/tenants/?tenant_name=${encodedTenantN}');
+        const roomsResponse = await axios.get('http://127.0.0.1:8000/api/rooms/');
+        const tenantsResponse = await axios.get('http://127.0.0.1:8000/api/tenants/');
         setRooms(roomsResponse.data);
         setTenants(tenantsResponse.data);
       } catch (error) {
@@ -66,8 +66,6 @@ const Booking = () => {
         isClosable: true,
       });
 
-     
-
       setFormData({
         room: '',
         tenant: '',
@@ -95,12 +93,22 @@ const Booking = () => {
           <form onSubmit={handleSubmit}>
             <FormControl id="room" mb={4}>
               <FormLabel>Room</FormLabel>
-            <Input type="name" name="name" value={formData.check_in_date} onChange={handleChange} />
+              <Select name="room" value={formData.room} onChange={handleChange}>
+                <option value="">Select Room</option>
+                {rooms.map(room => (
+                  <option key={room.id} value={room.id}>{room.number}</option>
+                ))}
+              </Select>
             </FormControl>
 
             <FormControl id="tenant" mb={4}>
               <FormLabel>Tenant</FormLabel>
-            <Input type="name" name="name" value={formData.check_in_date} onChange={handleChange} />
+              <Select name="tenant" value={formData.tenant} onChange={handleChange}>
+                <option value="">Select Tenant</option>
+                {tenants.map(tenant => (
+                  <option key={tenant.id} value={tenant.id}>{tenant.name}</option>
+                ))}
+              </Select>
             </FormControl>
 
             <FormControl id="check_in_date" mb={4}>
@@ -113,9 +121,7 @@ const Booking = () => {
               <Input type="date" name="check_out_date" value={formData.check_out_date} onChange={handleChange} />
             </FormControl>
 
-            <Button type="submit"   bg="#0097b2"
-              color="white"
-              _hover={{ bg: "#073d47" }} width="full" mt={4}>
+            <Button type="submit" bg="#0097b2" color="white" _hover={{ bg: "#073d47" }} width="full" mt={4}>
               Create Booking
             </Button>
           </form>
@@ -126,19 +132,18 @@ const Booking = () => {
       </GridItem>
       
       <GridItem>
-        <Box borderWidth={1} borderRadius="lg" p={5} textAlign="center" >
-         <Box 
-        w="700px"
-        p={6}
-        height="340px"
-        bg="gray.100"
-        boxShadow="lg"
-        bgSize="cover"
-        bgPosition="center"
-        mr={6} 
-        style={{ backgroundImage: `url(${backgroundImage})` }}
-      />
-
+        <Box borderWidth={1} borderRadius="lg" p={5} textAlign="center">
+          <Box 
+            w="700px"
+            p={6}
+            height="340px"
+            bg="gray.100"
+            boxShadow="lg"
+            bgSize="cover"
+            bgPosition="center"
+            mr={6} 
+            style={{ backgroundImage: `url(${backgroundImage})` }}
+          />
           <Box mt={4} fontWeight="bold">
             Book Your Stay
           </Box>
