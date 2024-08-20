0e31032eb0d46c313bd169aa4cca1a7af0e04661
Zawadirugenyi <zawadirugenyi@gmail.com>
2024-08-20 15:49:37 +0300

    booking

--- src/pages/booking.jsx
+++ src/pages/booking.jsx
@@ -117,6 +117,7 @@ const Booking = () => {
 
   return (
     <Box display="flex" justifyContent="center" mt={10}>
+      
       <Box w="600px" p={6} bg="white" boxShadow="lg" rounded="md" borderWidth="1px" borderRadius="lg" overflow="hidden">
         <Heading mb={6}>Book a Room</Heading>
         {message.text && (
