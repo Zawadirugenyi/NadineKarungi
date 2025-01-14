7e50c41e2a1dc628f1e977d03b7e7df07337b77a
Zawadirugenyi <zawadirugenyi@gmail.com>
2025-01-14 15:19:31 +0200

    fix

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,7 +9,7 @@ import { Box, Button, Heading, Grid,Text,Card, CardBody ,Image,
 import backgroundImage1 from '../Components/Assetes/home1.webp';
 import backgroundImage2 from '../Components/Assetes/111.webp';
 import backgroundImage3 from '../Components/Assetes/home3.jpg';
-import aboutImage from '../Components/Assetes/Gerante1-removebg-preview.png'
+import aboutImage from '../Components/Assetes/Gerante1.jpeg'
 import backgroundImage4 from '../Components/Assetes/1113.jpeg';
 import { FaWhatsapp } from 'react-icons/fa'; 
 import { useTranslation } from 'react-i18next'; 
